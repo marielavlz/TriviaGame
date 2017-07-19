755606508d1e8e795c7f5a51546f4306135a82fc
Mariela Velez <Mariela@Marielas-MacBook-Air.local>
2017-07-18 23:44:43 -0500

    Changed it to showing one question at a time. Still need to add a timer and events related to the timer. Also want to change the html and css to make it prettier.

--- assets/javascript/quiz.js
+++ assets/javascript/quiz.js
@@ -5,68 +5,7 @@ $(document).ready(function(){
 
 (function() {
   
-  function buildQuiz() {
-  var output = [];
-
-    // for each question store a list of choices
-    myQuestions.forEach((currentQuestion, questionNumber) => {
-      var answers = [];
-
-      // and for each available answer...
-      for (letter in currentQuestion.answers) {
-        // add an HTML radio button
-        answers.push(
-          `<label>
-            <input type="radio" name="question${questionNumber}" value="${letter}">
-            ${letter} :
-            ${currentQuestion.answers[letter]}
-          </label>`
-        );
-      }
-
-      // add this question and its answers to the output
-      output.push(
-        `<div class="question"> ${currentQuestion.question} </div>
-        <div class="answers"> ${answers.join("")} </div>`
-      );
-    });
-
-    // finally combine our output list into one string of HTML and put it on the page
-    quizContainer.innerHTML = output.join("");
-  };
-
-  function showResults() {
-    // gather answer containers from our quiz
-    var answerContainers = quizContainer.querySelectorAll(".answers");
-
-    // keep track of user's answers
-    var numCorrect = 0;
-
-    // for each question find an answer
-    myQuestions.forEach((currentQuestion, questionNumber) => {
-      var answerContainer = answerContainers[questionNumber];
-      var selector = `input[name=question${questionNumber}]:checked`;
-      var userAnswer = (answerContainer.querySelector(selector) || {}).value;
-
-      // if answer is correct
-      if (userAnswer === currentQuestion.correctAnswer) {
-        // add to the number of correct answers
-        numCorrect++;
-
-        $("#results").append(numCorrect);
-
-      }
-    })
-
-    // show number of correct answers out of total
-    resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
-  };
-
-  var quizContainer = document.getElementById("quiz");
-  var resultsContainer = document.getElementById("results");
-  var submitButton = document.getElementById("submit");
-
-  var myQuestions = [
+   var myQuestions = [
     {
       question: "Who is Jon Snow's father?",
       answers: {
@@ -108,14 +47,14 @@ $(document).ready(function(){
       correctAnswer: "a"
     },
     {
-      question: "How many White Walkers/Others has Jon Snow killed in the series?",
+      question: "Who was Hand of the King during Joffrey's reign?",
       answers: {
-        a: "None",
-        b: "One",
-        c: "Two",
-        d: "Three"
+        a: "Tywin Lannister",
+        b: "Tyrion Lannister",
+        c: "Ned Stark",
+        d: "Two of the Above"
       },
-      correctAnswer: "c"
+      correctAnswer: "d"
     },
     {
       question: "Who was the last Targaryen king of Westoros?",
@@ -129,16 +68,125 @@ $(document).ready(function(){
     }
   ];
 
+  function buildQuiz() {
+    // store the HTML output
+    var output = [];
+
+    // Store a list of choices for each question.
+    myQuestions.forEach((currentQuestion, questionNumber) => {
+      var answers = [];
+
+      // and for each available answer add an HTML radio button to prevent more than one choice.
+      for (letter in currentQuestion.answers) {
+        answers.push(
+          `<label>
+             <input type="radio" name="question${questionNumber}" value="${letter}">
+              ${letter} :
+              ${currentQuestion.answers[letter]}
+           </label>`
+        );
+      }
+
+      // add this question and its answers to the output
+      output.push(
+        `<div class="slide">
+           <div class="question"> ${currentQuestion.question} </div>
+           <div class="answers"> ${answers.join("")} </div>
+         </div>`
+      );
+    });
+
+    // combine our output list into one string of HTML and put it on the page
+    quizContainer.innerHTML = output.join("");
+  }
+
+  function showResults() {
+    // gather answer containers from our quiz
+    var answerContainers = quizContainer.querySelectorAll(".answers");
+
+    // keep track of user's answers
+    let numCorrect = 0;
+
+    // for each question...
+    myQuestions.forEach((currentQuestion, questionNumber) => {
+      // find selected answer
+      var answerContainer = answerContainers[questionNumber];
+      var selector = `input[name=question${questionNumber}]:checked`;
+      var userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+      // if answer is correct
+      if (userAnswer === currentQuestion.correctAnswer) {
+        // add to the number of correct answers
+        numCorrect++;
+
+      } else {
+        // if answer is wrong or blank
+        //I should add something here but Idk yet
+      }
+    });
+
+    // show number of correct answers out of total
+    resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+  }
+
+  function showSlide(n) {
+    slides[currentSlide].classList.remove("active-slide");
+    slides[n].classList.add("active-slide");
+    currentSlide = n;
+    
+    if (currentSlide === 0) {
+      previousButton.style.display = "none";
+    } else {
+      previousButton.style.display = "inline-block";
+    }
+    
+    if (currentSlide === slides.length - 1) {
+      nextButton.style.display = "none";
+      submitButton.style.display = "inline-block";
+    } else {
+      nextButton.style.display = "inline-block";
+      submitButton.style.display = "none";
+    }
+  }
+
+  function showNextSlide() {
+    showSlide(currentSlide + 1);
+  }
+
+  function showPreviousSlide() {
+    showSlide(currentSlide - 1);
+  }
+
+  var quizContainer = document.getElementById("quiz");
+  var resultsContainer = document.getElementById("results");
+  var submitButton = document.getElementById("submit");
+
   // display quiz right away
   buildQuiz();
 
-  // on submit, show results
+  var previousButton = document.getElementById("previous");
+  var nextButton = document.getElementById("next");
+  var slides = document.querySelectorAll(".slide");
+  let currentSlide = 0;
 
-  $("#submit").on("click", function() {
-    showResults();
+  showSlide(0);
+
+  // on submit, show results
+  //submitButton.addEventListener("click", showResults);
+  //previousButton.addEventListener("click", showPreviousSlide);
+  //nextButton.addEventListener("click", showNextSlide);
+
+  $("#next").on("click", function(){
+    showNextSlide();
   });
-  
+  $("#previous").on("click", function(){
+    showPreviousSlide();
+  });
+  $("#submit").on("click", function(){
+    showResults();
+  })
 })();
+
 
 });
 
@@ -149,3 +197,4 @@ $(document).ready(function(){
   //Upon clicking submit, the answer choices selected are compared to the correct answer choices.
   //A tally is taken of correct responses and incorrect responses without specifying which question was answered correctly or incorrectly
   //The results are then displayed at the bottom of the page or somehow whenever
+ 
